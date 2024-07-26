790996a69b571ae1f13c4bef883d2a252db15e04
Jaden Soroka <jaden.c.soroka@sherwin.com>
2024-07-26 15:38:49 -0400

    282 complete

--- 36. React.js/my-react-app/src/App.jsx
+++ 36. React.js/my-react-app/src/App.jsx
@@ -1,14 +1,10 @@
 
 
 export default function App() {
+
   return (
     <>
-      <h1>My favorite foods</h1>
-      <ul>
-        <li>Steak</li>
-        <li>Lobster</li>
-        <li>Jelly Beans</li>
-      </ul>
+
     </>
   );
 }
--- 36. React.js/my-react-app/src/main.jsx
+++ 36. React.js/my-react-app/src/main.jsx
@@ -1,6 +1,17 @@
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+// import App from './App.jsx';
 import './index.css';
 
+const fName = 'Jaden';
+const lName = 'Soroka';
+
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+
+root.render(
+    <div>
+        <h1>Hello {fName} {lName}</h1>
+        <p>Your lucky number is {Math.floor(Math.random() * 9)}</p>
+    </div>
+);
+
+// root.render(<App />);
