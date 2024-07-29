758af9b228924f3797760d6c817f0137e17b25c2
Jaden Soroka <jaden.c.soroka@sherwin.com>
2024-07-29 14:38:34 -0400

    Keeper app part 1

--- 36. React.js/my-react-app/src/main.jsx
+++ 36. React.js/my-react-app/src/main.jsx
@@ -1,17 +1,9 @@
 import { createRoot } from 'react-dom/client';
-// import App from './App.jsx';
+import App from './App.jsx';
 import './index.css';
-
-const name = 'Jaden Soroka';
-const year = new Date().getFullYear();
 
 const root = createRoot(document.getElementById('root'));
 
-root.render(
-    <div>
-        <p>Created by {name}</p>
-        <p>Copyright @ {year}</p>
-    </div>
-);
 
-// root.render(<App />);
+
+root.render(<App />);
