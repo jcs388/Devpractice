035f7eaeb84b2736b6a2d938844f0be81eef13e8
Jaden Soroka <jaden.c.soroka@sherwin.com>
2024-07-31 09:52:54 -0400

    Keeper app project - part 2 complete

--- 36. React.js/Keeper Project/src/App.jsx
+++ 36. React.js/Keeper Project/src/App.jsx
@@ -1,13 +1,27 @@
 import Header from "./Header"
 import Footer from "./Footer"
 import Note from "./Note"
+import notes from "./notes"
+
+function createNote(notes) {
+  return (
+    <Note 
+      key={notes.key}
+      title={notes.title}
+      content={notes.content}
+    />
+  )
+}
 
 export default function App() {
-
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(notes => ( <Note 
+      key={notes.key}
+      title={notes.title}
+      content={notes.content}
+    /> ))}
       <Footer />
     </div>
   );
